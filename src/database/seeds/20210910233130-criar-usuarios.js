4e4645ef6184b0f9d3f81926c61046f4479c0cbc
alan <al_coimbra@hotmail.com>
2021-09-13 20:40:49 -0300

    Criacao da rota de fotos e inserir fotos no banco de dados

--- src/database/seeds/20210910233130-criar-usuarios.js
+++ src/database/seeds/20210910233130-criar-usuarios.js
@@ -5,22 +5,22 @@ module.exports = {
     'users',
     [
       {
-        nome: 'Alan 1',
-        email: 'al_coimbra1@hotmail.com',
+        nome: 'Alan 5',
+        email: 'al_coimbra6@hotmail.com',
         password_hash: await bcryptjs.hash('123456', 8),
         created_at: new Date(),
         updated_at: new Date(),
       },
       {
-        nome: 'Aline 2',
-        email: 'aline2@hotmail.com',
+        nome: 'Aline 4',
+        email: 'aline7@hotmail.com',
         password_hash: await bcryptjs.hash('123456', 8),
         created_at: new Date(),
         updated_at: new Date(),
       },
       {
-        nome: 'Henrique 3',
-        email: 'henrique3@hotmail.com',
+        nome: 'Henrique 5',
+        email: 'henrique6@hotmail.com',
         password_hash: await bcryptjs.hash('123456', 8),
         created_at: new Date(),
         updated_at: new Date(),
