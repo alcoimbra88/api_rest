7a4f5042f76ae721a36f8c91bbcbd2588d14abb0
alan <al_coimbra@hotmail.com>
2021-09-15 16:27:37 -0300

    Adicionando arquivos

---
+++ dist/database/seeds/20210910233130-criar-usuarios.js
@@ -0,0 +1,33 @@
+"use strict";const bcryptjs = require('bcryptjs');
+
+module.exports = {
+  up: async (queryInterface) => queryInterface.bulkInsert(
+    'users',
+    [
+      {
+        nome: 'Alan 5',
+        email: 'al_coimbra6@hotmail.com',
+        password_hash: await bcryptjs.hash('123456', 8),
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        nome: 'Aline 4',
+        email: 'aline7@hotmail.com',
+        password_hash: await bcryptjs.hash('123456', 8),
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        nome: 'Henrique 5',
+        email: 'henrique6@hotmail.com',
+        password_hash: await bcryptjs.hash('123456', 8),
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+    ],
+    {},
+  ),
+
+  down: () => {},
+};
