c9f6ab7565c2d45ab7aac599cec3e3a5ad443f4c
alan <al_coimbra@hotmail.com>
2021-10-04 14:45:02 -0300

    Abertura da rota de store

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -8,7 +8,7 @@ const router = new Router();
 // router.get('/', loginRequired, userController.index); // Lista usuarios
 // router.get('/:id', userController.show); // Lista usuario
 
-router.post('/', loginRequired, userController.store);
+router.post('/', userController.store);
 router.put('/', loginRequired, userController.update);
 router.delete('/', loginRequired, userController.delete);
 
